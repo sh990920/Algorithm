dc9cee84202ccc2e6683a2493bcf8a230f182abc
sh990920 <107488193+sh990920@users.noreply.github.com>
2024-04-18 16:43:43 +0900

    [level 1] Title: 모의고사, Time: 3.63 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    var result = Array<Int>()
+    
+    let aList = [1, 2, 3, 4, 5]
+    let aLen = aList.count
+    let bList = [2, 1, 2, 3, 2, 4, 2, 5]
+    let bLen = bList.count
+    let cList = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    let cLen = cList.count
+    var dict = [1: 0, 2: 0, 3: 0]
+    
+    let len = answers.count
+    
+    for i in 0 ..< len {
+        if i == 0 {
+            if answers[i] == aList[i] {
+                dict[1]! += 1
+            }
+            if answers[i] == bList[i] {
+                dict[2]! += 1
+            }
+            if answers[i] == cList[i] {
+                dict[3]! += 1
+            }
+        } else {
+            if answers[i] == aList[i % aLen] {
+                dict[1]! += 1
+            }
+            if answers[i] == bList[i % bLen] {
+                dict[2]! += 1
+            }
+            if answers[i] == cList[i % cLen] {
+                dict[3]! += 1
+            }
+        }
+    }
+    
+    let maxNum = max(dict[1]! ,dict[2]!, dict[3]!)
+    
+    for key in dict.keys {
+        if dict[key]! == maxNum{
+            result.append(key)
+        }
+    }
+    
+    result.sort()
+    
+    return result
+}
