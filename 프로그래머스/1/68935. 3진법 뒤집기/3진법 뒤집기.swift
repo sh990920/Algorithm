30cbb5ec4406dd313af5b7afab76a258a99e0935
sh990920 <107488193+sh990920@users.noreply.github.com>
2024-05-03 13:57:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,39 +1,46 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var result = 0
-    var three: String = ""
-    var num = 3
-    var numCount = 1
-    var N = n
+//     var result = 0
+//     var three: String = ""
+//     var num = 3
+//     var numCount = 1
+//     var N = n
     
-    while (N > 0) {
-        if (N >= num) {
-            num *= 3
-            numCount += 1
-            continue
-        } else {
-            num /= 3
-            var cnt = N / num
-            three += String(cnt)
-            N = N % num
-        }
+//     while (N > 0) {
+//         if (N >= num) {
+//             num *= 3
+//             numCount += 1
+//             continue
+//         } else {
+//             num /= 3
+//             var cnt = N / num
+//             three += String(cnt)
+//             N = N % num
+//         }
+//     }
+//     if three.count < numCount {
+//         for i in 0 ..< numCount - three.count {
+//             three += "0"
+//         }
+//     }
+//     var cnt = 0
+//     for i in three {
+//         if let res = Int(String(i)) {
+//             if cnt == 0 {
+//                 result += res
+//             } else {
+//                 result += Int(Double(res) * pow(Double(3), Double(cnt)))
+//             }
+//         }
+//         cnt += 1
+//     }
+//     return result
+    var str = String(n, radix: 3)
+    var str2 = ""
+    
+    for i in str.reversed() {
+        str2 += String(i)
     }
-    if three.count < numCount {
-        for i in 0 ..< numCount - three.count {
-            three += "0"
-        }
-    }
-    var cnt = 0
-    for i in three {
-        if let res = Int(String(i)) {
-            if cnt == 0 {
-                result += res
-            } else {
-                result += Int(Double(res) * pow(Double(3), Double(cnt)))
-            }
-        }
-        cnt += 1
-    }
-    return result
+    return Int(str2, radix: 3)!
 }
