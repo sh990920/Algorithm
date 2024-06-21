acc739db4bc3126bca95b59c0cbb3c317a068c43
sh990920 <107488193+sh990920@users.noreply.github.com>
2024-06-21 09:46:49 +0900

    [level 2] Title: 할인 행사, Time: 0.11 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
+    var count = number.reduce(0, +)
+    var dict = [String: Int]()
+    var result = Array<Int>()
+    
+    for i in 0..<want.count {
+        dict[want[i]] = i
+    }
+    
+    for i in 0...discount.count - count {
+        var arr = discount[i..<i+count].map{ dict[$0] }
+        var check = Array(repeating: 0, count: want.count)
+        
+        if arr.contains(nil){
+            continue
+        } else {
+            for j in arr {
+                check[j!] += 1
+            }
+            if check == number {
+                result.append(i+1)
+            }
+        }
+        
+        
+    }
+    
+    
+    return result.count
+}
