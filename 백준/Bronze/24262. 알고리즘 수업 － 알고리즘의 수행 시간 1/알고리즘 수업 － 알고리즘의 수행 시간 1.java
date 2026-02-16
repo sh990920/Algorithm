f7cb7d1193bbbfd47aac16447b7a7b8f88eb0b65
Seunghwan <107488193+sh990920@users.noreply.github.com>
2026-02-16 16:48:48 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public void solution() throws Exception {
+        System.out.println(1);
+        System.out.println(0);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
